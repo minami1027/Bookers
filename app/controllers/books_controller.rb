50f2eb7720aa5f3c876e6f48bede7156aa3bd383
minami1027 <minamimohumohu@gmail.com>
2019-08-11 16:45:57 +0900

    initial commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,58 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def new
+  end
+
+  def create
+  	book = Book.new(book_params)
+    if book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(book.id)
+    else
+    @books = Book.all
+    @book = Book.new
+      render :index
+    end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+    if book.save
+    flash[:notice] = "Book was successfully updated."
+    book.update(book_params)
+    redirect_to book_path(book.id)
+    else
+    @books = Book.all
+    @book = Book.new
+    render :index
+    end
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	if book.save
+    flash[:notice] = "Book was successfully destroyed."
+    book.destroy
+    redirect_to books_path
+    end
+  end
+
+  private
+  def book_params
+  params.require(:book).permit(:title, :body)
+  end
+
+end
